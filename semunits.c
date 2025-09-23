fb7784aa9277a50fc6c5bf7b4cd18d13ed3e9533
Jonnie-Media <johnkiharamwangi2@gmail.com>
2025-09-23 22:26:08 +0300

    Add files via upload

---
+++ semunits.c
@@ -0,0 +1,24 @@
+//Display units for this semester
+/*
+Name:John
+Reg:133
+Date:20/9/2025
+Description: Units for this semester program 
+
+*/
+#include <stdio.h>
+//main function
+ 
+int main () 
+{
+	printf("Units for this semester \n");
+	printf("1.ucu2105 Fundamentals of ICT \n");
+	printf("2.SPC2102 Introduction to computer architecture \n"); 
+	printf("3.ucu2101 Communication skills \n");
+	printf("4.SPH2107 Analogue electronics \n");
+	printf("5.SPM2100 Diacrete mathematics \n");
+	printf("6.SPM2104 Mathematics for science \n"); 
+	printf("7.SPH2101 Pysics\n");
+	printf("8.SPC2101 Introduction to programming and problem solving \n");
+    return 0;
+}
